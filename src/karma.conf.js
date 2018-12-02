85c230fb2d10a981155055e6fa6e55da1821a936
Laura-Weber <laura.weber57@laposte.net>
2018-12-02 20:12:42 +0100

    test28

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -26,6 +26,12 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+      customLaunchers: {
+          ChromeHeadlessNoSandbox: {
+              base: 'Chrome',
+              flags: ['--no-sandbox']
+          }
+      },
     singleRun: false
   });
 };
