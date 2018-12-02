9ec2cbab08ba2214f80868fd298e928090f6cf2b
Laura-Weber <laura.weber57@laposte.net>
2018-12-02 21:59:39 +0100

    test39

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('phantomjs-prebuilt'),
+      require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -25,10 +25,10 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['PhantomJS'],
+    browsers: ['ChromeHeadless'],
       customLaunchers: {
           ChromeNoSandbox: {
-              base: 'PhantomJS',
+              base: 'ChromeHeadless',
               flags: ['--no-sandbox']
           }
       },
